d464889173b136bd302d2c30b7f96076c5363129
bird <bird@birddeMacBook-Pro.local>
2026-07-14 03:04:04 +0900

    fix: preserve admin token for role switching

--- web/src/api.js
+++ web/src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({ baseURL: '/' })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token && !config.headers.Authorization) config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
